7ab8568042dae812d17b82753b54e15bd2fe4ff6
Nayama Rajlich <design@nayamarajlich.com>
2023-09-24 16:08:35 -0300

    icon Link

--- components/iconb.js
+++ components/iconb.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Iconb() {
-    return <a className="text-xl font-bold font-akatab" href="#">
+    return <Link className="text-xl font-bold font-akatab" href="/">
         <img src="images/blogo.png" alt="Logo"
             className="inline-block"
             style={{ height: '30px', width: 'auto', position: 'relative', top: '-4px', marginLeft: '20px', marginRight: '2px' }} />ubble
         Brain
-    </a>
+    </Link>
 }
